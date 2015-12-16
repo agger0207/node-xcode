85e9157f65c8d6b334da11bdf4f17096d5a1fd23
Wangliping <netease@NetEasedeiMac-3.local>
2015-12-16 17:59:36 +0800

    恢复测试工程

--- test/HTJSGeneratorCode/Integrate.js
+++ test/HTJSGeneratorCode/Integrate.js
@@ -17,11 +17,18 @@ myProj.parse(function (err) {
     myProj.addHeaderFile('Models/HTTestModel.h', {}, keyByName);
     myProj.addFile('Models/HTTestModel.m', keyByName, {});
 
+
     //myProj.removeFile('Models/HTTestModel.h', {}, 'HTJSGeneratorCode');
     //myProj.removeFile('Models/HTTestModel.m', 'HTJSGeneratorCode', {});
     //
     //myProj.addHeaderFile('Models/HTTestModel.h', {}, 'HTJSGeneratorCode');
     //myProj.addFile('Models/HTTestModel.m', 'HTJSGeneratorCode', {});
+
+    var modelGroupKey = myProj.pbxCreateGroup("Models", "HTJSGeneratorCode/Models");
+    var requestGroupKey = myProj.pbxCreateGroup("Requests", "HTJSGeneratorCode/Models");
+    console.log("modelGroupKey: " + modelGroupKey + " requestGroupKey: " + requestGroupKey);
+
+    myProj.addFile('Requests/HTTestRequest.m', requestGroupKey, {});
 
     fs.writeFileSync(projectPath, myProj.writeSync());
     console.log('new project written 111');
