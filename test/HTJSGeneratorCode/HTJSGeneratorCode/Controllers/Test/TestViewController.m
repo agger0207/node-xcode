be230f688d8ef3c1678da5674411848d7db2eaeb
Wangliping <hzwangliping@corp.netease.com>
2015-12-24 11:38:37 +0800

    去除一些无用的注释

---
+++ test/HTJSGeneratorCode/HTJSGeneratorCode/Controllers/Test/TestViewController.m
@@ -0,0 +1,13 @@
+//
+//  TestViewController.m
+//  HTJSGeneratorCode
+//
+//  Created by Wangliping on 15/12/21.
+//  Copyright © 2015年 Netease. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@implementation TestViewController
+
+@end
